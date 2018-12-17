51a41420258810fd0e50e0e839c17a9f4c287a9d
mine-lee <mine_lee@qq.com>
2018-12-17 23:22:03 +0800

    测试bus

--- warriors-server-basic/warriors-config-center/src/main/java/com/mine/warriorsconfigcenter/WarriorsConfigCenterApplication.java
+++ warriors-server-basic/warriors-config-center/src/main/java/com/mine/warriorsconfigcenter/WarriorsConfigCenterApplication.java
@@ -1,32 +1,20 @@
 package com.mine.warriorsconfigcenter;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 @EnableEurekaClient
-@RefreshScope
-@RestController
 public class WarriorsConfigCenterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WarriorsConfigCenterApplication.class, args);
     }
 
-    @Value("${server.port}")
-    private String port;
-
-    @RequestMapping("hi")
-    public String hi() {
-        return "服务器中的端口为" + port;
-    }
 }
 
