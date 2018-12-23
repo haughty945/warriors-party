b5024284818e9723959c0409eb33e1cf09cbbb5b
mine-lee <mine_lee@qq.com>
2018-12-23 23:32:46 +0800

    添加RabbitMQ相关pom依赖

--- warriors-server-integration/warriors-server-rabbitMQ/src/main/java/com/mine/warriorsserverrabbitmq/WarriorsServerRabbitMQApplication.java
+++ warriors-server-integration/warriors-server-rabbitMQ/src/main/java/com/mine/warriorsserverrabbitmq/WarriorsServerRabbitMQApplication.java
@@ -2,6 +2,8 @@ package com.mine.warriorsserverrabbitmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +13,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class WarriorsServerRabbitMQApplication {
 
     public static void main(String[] args) {
