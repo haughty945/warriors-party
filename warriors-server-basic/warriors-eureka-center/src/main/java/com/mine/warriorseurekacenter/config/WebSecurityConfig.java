456c9ea55fa59865c7ec70f8ecdb83723a6d6d21
ming.li <ming.li@timanetworks.com>
2018-12-29 16:19:10 +0800

    服务注册与发现加入security

--- warriors-server-basic/warriors-eureka-center/src/main/java/com/mine/warriorseurekacenter/config/WebSecurityConfig.java
+++ warriors-server-basic/warriors-eureka-center/src/main/java/com/mine/warriorseurekacenter/config/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-    	//http.csrf().ignoringAntMatchers("/eureka/**");
-        //super.configure(http);
+    	http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
     }
 }
--- warriors-server-common/warriors-server-mybatis-plus/src/main/java/com/mine/warriorsservermybatisplus/WarriorsServerMybatisPlusApplication.java
+++ warriors-server-common/warriors-server-mybatis-plus/src/main/java/com/mine/warriorsservermybatisplus/WarriorsServerMybatisPlusApplication.java
@@ -2,15 +2,11 @@ package com.mine.warriorsservermybatisplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
-@RestController
 @RefreshScope
 public class WarriorsServerMybatisPlusApplication {
 
