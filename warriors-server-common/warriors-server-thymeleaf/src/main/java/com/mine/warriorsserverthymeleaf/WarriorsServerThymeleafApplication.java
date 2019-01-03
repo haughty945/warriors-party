80b0b8b1501d436db9f99bf7684dea270d59ee01
ming.li <ming.li@timanetworks.com>
2019-01-03 18:00:40 +0800

    整合thymeleaf组件

--- warriors-server-common/warriors-server-thymeleaf/src/main/java/com/mine/warriorsserverthymeleaf/WarriorsServerThymeleafApplication.java
+++ warriors-server-common/warriors-server-thymeleaf/src/main/java/com/mine/warriorsserverthymeleaf/WarriorsServerThymeleafApplication.java
@@ -2,8 +2,6 @@ package com.mine.warriorsserverthymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,13 +9,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
-//@RefreshScope
-public class WarriorsServerThymeleafApplication extends SpringBootServletInitializer {
-
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return builder.sources(WarriorsServerThymeleafApplication.class);
-//    }
+@RefreshScope
+public class WarriorsServerThymeleafApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WarriorsServerThymeleafApplication.class, args);
