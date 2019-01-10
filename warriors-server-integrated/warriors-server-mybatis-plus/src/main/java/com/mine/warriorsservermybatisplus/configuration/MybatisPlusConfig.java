a841d4bd0ea4f2c46027c280a6d67bda5f4493ee
mine-lee <mine_lee@qq.com>
2019-01-10 23:18:29 +0800

    redis工程配置文件修改

--- warriors-server-integrated/warriors-server-mybatis-plus/src/main/java/com/mine/warriorsservermybatisplus/configuration/MybatisPlusConfig.java
+++ warriors-server-integrated/warriors-server-mybatis-plus/src/main/java/com/mine/warriorsservermybatisplus/configuration/MybatisPlusConfig.java
@@ -2,10 +2,13 @@ package com.mine.warriorsservermybatisplus.configuration;
 
 import com.baomidou.mybatisplus.mapper.ISqlInjector;
 import com.baomidou.mybatisplus.mapper.LogicSqlInjector;
+import com.baomidou.mybatisplus.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 //Spring boot方式
@@ -28,5 +31,22 @@ public class MybatisPlusConfig {
     @Bean
     public ISqlInjector sqlInjector() {
         return new LogicSqlInjector();
+    }
+
+    /**
+     * 乐观锁
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * SQL执行效率插件
+     */
+    @Bean
+    @Profile({"dev"})// 设置 dev 环境开启
+    public PerformanceInterceptor performanceInterceptor() {
+        return new PerformanceInterceptor();
     }
 }
