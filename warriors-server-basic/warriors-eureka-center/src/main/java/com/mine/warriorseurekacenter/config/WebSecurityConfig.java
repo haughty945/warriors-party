dea47d433308a1d2c52dbfc3b427e43f05423c99
ming.li <ming.li@timanetworks.com>
2019-01-10 16:13:04 +0800

    添加admin监控

--- warriors-server-basic/warriors-eureka-center/src/main/java/com/mine/warriorseurekacenter/config/WebSecurityConfig.java
+++ warriors-server-basic/warriors-eureka-center/src/main/java/com/mine/warriorseurekacenter/config/WebSecurityConfig.java
@@ -1,21 +1,22 @@
 package com.mine.warriorseurekacenter.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- *
  * @program: pluto-program
  * @description: 配置security的csrf检验为false
- *
- * */
+ */
 @EnableWebSecurity
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-    	http.csrf().ignoringAntMatchers("/eureka/**");
+        http.csrf().ignoringAntMatchers("/eureka/**");
         super.configure(http);
     }
 }
