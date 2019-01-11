92024bc3c9283cda91c643ea5cab8e7f0a9277a0
mine-lee <mine_lee@qq.com>
2019-01-12 00:15:14 +0800

    整体配合admin监控做出相应的log调整

--- warriors-server-integrated/warriors-server-ribbon/src/main/java/com/mine/warriorsserverribbon/WarriorsServerRibbonApplication.java
+++ warriors-server-integrated/warriors-server-ribbon/src/main/java/com/mine/warriorsserverribbon/WarriorsServerRibbonApplication.java
@@ -3,13 +3,13 @@ package com.mine.warriorsserverribbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableHystrix
+@RefreshScope
 public class WarriorsServerRibbonApplication {
 
     public static void main(String[] args) {
