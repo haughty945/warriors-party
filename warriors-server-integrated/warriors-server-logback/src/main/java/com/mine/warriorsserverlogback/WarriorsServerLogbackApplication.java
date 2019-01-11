1b28af508d3f98e4177d536daebfdb086fdc7b87
mine-lee <mine_lee@qq.com>
2019-01-12 00:58:14 +0800

    logback 注册到eureka

--- warriors-server-integrated/warriors-server-logback/src/main/java/com/mine/warriorsserverlogback/WarriorsServerLogbackApplication.java
+++ warriors-server-integrated/warriors-server-logback/src/main/java/com/mine/warriorsserverlogback/WarriorsServerLogbackApplication.java
@@ -2,8 +2,10 @@ package com.mine.warriorsserverlogback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WarriorsServerLogbackApplication {
 
     public static void main(String[] args) {
