ca6852576d9e60d74a15d05fbaec3d9c39f2057a
mine-lee <mine_lee@qq.com>
2019-01-13 04:23:59 +0800

    测试redis分布式锁

--- warriors-server-basic/warriors-gateway-zuul/src/main/java/com/mine/warriorsgateway/WarriorsGateWayApplication.java
+++ warriors-server-basic/warriors-gateway-zuul/src/main/java/com/mine/warriorsgateway/WarriorsGateWayApplication.java
@@ -2,12 +2,14 @@ package com.mine.warriorsgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
+@RefreshScope
 public class WarriorsGateWayApplication {
 
     public static void main(String[] args) {
