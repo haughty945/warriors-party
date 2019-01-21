f7381c6bb820ffdeaadf6c92b960f2a5e2c84b84
ming.li <ming.li@timanetworks.com>
2019-01-21 19:57:25 +0800

    添加mongodb工程

---
+++ warriors-server-integrated/warriors-server-mongodb-plus/src/main/java/com/mine/warriorsservermongodb/WarriorsServerMongodbApplication.java
@@ -0,0 +1,14 @@
+package com.mine.warriorsservermongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarriorsServerMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarriorsServerMongodbApplication.class, args);
+    }
+
+}
+
