883d585031871e552d6e96a1d981c59b7d1fbdac
ming.li <ming.li@timanetworks.com>
2019-01-28 16:22:59 +0800

    注册中心修改为localhost

--- warriors-server-basic/warriors-monitor-admin/src/main/java/com/mine/warriorsmonitoradmin/configuration/SecuritySecureConfig.java
+++ warriors-server-basic/warriors-monitor-admin/src/main/java/com/mine/warriorsmonitoradmin/configuration/SecuritySecureConfig.java
@@ -1,18 +1,10 @@
 package com.mine.warriorsmonitoradmin.configuration;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @Program: warriors-party
