32626bc6a9c9976e049980a289ef9731197fac1a
ming.li <ming.li@timanetworks.com>
2019-02-26 19:31:08 +0800

    excel 项目添加

---
+++ warriors-server-integrated/warriors-server-esay-excel/src/test/java/com/mine/warriorsserveresayexcel/WarriorsServerEsayExcelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mine.warriorsserveresayexcel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WarriorsServerEsayExcelApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
