7294e156b49e611ed6c7555b38ced5c37b2d184f
liming <liming@wenxiangcn.com>
2019-03-05 15:26:28 +0800

    删除冗余配置

---
+++ warriors-server-basic/warriors-gateway-center/src/main/java/com/mine/warriorsgatewaycenter/config/CorsConfig.java
@@ -0,0 +1,33 @@
+//package com.mine.warriorsgatewaycenter.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.util.pattern.PathPatternParser;
+//
+//
+///**
+// * @Program: warriors-party
+// * @Description: 跨域过滤器
+// * @Author: Mine.Lee
+// * @Create: 2019-02-15 15:42
+// * @Version: v1.0
+// */
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public CorsWebFilter corsFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.addAllowedMethod("*");
+//        config.addAllowedOrigin("*");
+//        config.addAllowedHeader("*");
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsWebFilter(source);
+//    }
+//}
