606a72ee26445b99e84ec6c46f36c2706973234a
liming <liming@wenxiangcn.com>
2019-03-26 11:19:19 +0800

    .

---
+++ warriors-server-integrated/warriors-server-freemarker/src/main/java/com/mine/warrirosserverfreemarker/WarrirosServerFreemarkerApplication.java
@@ -0,0 +1,13 @@
+package com.mine.warrirosserverfreemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarrirosServerFreemarkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarrirosServerFreemarkerApplication.class, args);
+    }
+
+}
