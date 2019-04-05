8e9efd9426dd92d0f7aa989682b7bdd7bed2ab57
mine-lee <M962464m>
2019-04-05 19:39:12 +0800

    修改服务器配置信息

--- warriors-server-common/src/main/java/com/mine/warriorsservercommon/config/exception/GlobalExceptionHandler.java
+++ warriors-server-common/src/main/java/com/mine/warriorsservercommon/config/exception/GlobalExceptionHandler.java
@@ -8,10 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,8 +22,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Slf4j
 @Controller
-@EnableWebMvc
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler implements ErrorController {
 
     /**
@@ -51,12 +47,8 @@ public class GlobalExceptionHandler implements ErrorController {
      * @create 2019/1/15 20:01
      */
     @RequestMapping("/error")
-    @ResponseBody
     public ResultVO NoHandlerFoundException(HttpServletRequest request) {
-        ResultVO ret = new ResultVO();
-        ret.setCode(404);
-        ret.setMsg("接口不存在");
-        return ret;
+        return new ResultVO(404,"接口不存在",null);
     }
 
     /**
@@ -70,13 +62,13 @@ public class GlobalExceptionHandler implements ErrorController {
      * @create 2019/1/15 20:03
      */
     @ExceptionHandler(value = Exception.class)
-    @ResponseBody
     public ResultVO ServletException(HttpServletRequest request, Exception e) {
         log.error("EXCEPTION_INTERCEPT" + " [ " + request.getRequestURI() + " ] " + "接口出现错误 , " + e.getMessage());
         ResultVO ret = new ResultVO();
         if (e instanceof HttpRequestMethodNotSupportedException) {
             ret.setCode(400);
             ret.setMsg("请求方式不正确");
+            ret.setData(e.getMessage());
         } else if (e instanceof HttpMessageNotReadableException || e instanceof HttpMediaTypeNotSupportedException || e instanceof MissingServletRequestParameterException) {
             ret.setCode(400);
             ret.setMsg("请求参数不正确");
